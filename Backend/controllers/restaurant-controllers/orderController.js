99da75f2fdc870866978785a1157bb36c0d63ff2
Rutz13 <ruturajjadhav456@gmail.com>
2024-03-29 23:13:44 +0530

    Create : Update Order Status and View Order History API completed

--- Backend/controllers/restaurant-controllers/orderController.js
+++ Backend/controllers/restaurant-controllers/orderController.js
@@ -39,4 +39,41 @@ exports.viewOrders = async (req, res) => {
             return res.status(400).json({ message: error.message });
         }
     };
+
+exports.updateOrderStatus = async (req, res) => {
+    try {
+        const { orderId, restaurantId } = req.body;
+        const order = await orderModel.findOne({ orderId: orderId, restaurantId: restaurantId });
+        order.orderStatus = 'Completed';
+        await order.save();
+        return res.status(200).json({ message: 'Order status updated successfully' });        
+    }catch(error){
+        return res.status(400).json({ message: error.message });
+    }
+};
+
+exports.viewOrderHistory = async (req, res) => {
+    try {
+        const { restaurantId } = req.body;
+
+        const ordersByRestaurant = await orderModel.aggregate([
+            {
+                $match: {
+                    restaurantId: new Types.ObjectId(restaurantId), // Filter by the specified restaurantId
+                    orderStatus: 'Completed' // Filter by the orderStatus
+                }
+            },
+            {
+                $project: {
+                    _id: 0, // Exclude the default _id field
+                    orderId: 1, // Include the orderId field
+                    orderTotal: 1 // Include the orderTotal field
+                }
+            }
+        ]);
+        return res.status(200).json({ ordersByRestaurant });
+    }catch(error){
+        return res.status(400).json({ message: error.message });
+    }
+};
   
--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,9 +1,11 @@
 const express =  require("express");
 
-const {viewOrders}  = require("../controllers/restaurant-controllers/orderController");
+const {viewOrders,updateOrderStatus,viewOrderHistory}  = require("../controllers/restaurant-controllers/orderController");
 
 const router = express.Router();
 
 router.post("/view", viewOrders);
+router.put("/update", updateOrderStatus);
+router.post("/history", viewOrderHistory);
 
 module.exports = router;
